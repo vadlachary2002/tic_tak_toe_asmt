a4b6ae680ff00fa86657203442176d49ba93c5c0
VADLACHARY <76141959+vadlachary2002@users.noreply.github.com>
2023-05-22 16:55:51 +0530

    Update Box.js

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "./Box.css"
 export const Box =  ({value,onClick}) => {
-  const boxStyle= value=="X"?"box x":"box o";       // for styling the x box and o box
+  const boxStyle= value==="X"?"box x":"box o";       // for styling the x box and o box
   return(
     <button className={boxStyle} onClick={onClick}>{value}</button>
 
